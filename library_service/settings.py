5ad358a5d8d8b80f901b6a1874dcfa9f68ea445a
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-06-13 16:55:14 +0200

    add djangorestframework

--- library_service/settings.py
+++ library_service/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom apps
+    "books",
+    "borrowings",
+    "user",
+    
+    # 3rd party apps
+    "rest_framework",
 ]
 
 MIDDLEWARE = [

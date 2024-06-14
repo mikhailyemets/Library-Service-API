0d4f62edced2ee9f41a4d0fca6b3e5ac23146983
Serhii Marynin <serhii.marynin@gmail.com>
2024-06-15 00:46:11 +0300

    Update BorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,19 +1,23 @@
 from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
 from borrowings.models import Borrowing
 from borrowings.serializers import (
     BorrowingSerializer,
     BorrowingListSerializer,
-    BorrowingRetrieveSerializer
+    BorrowingRetrieveSerializer,
+    BorrowingCreateSerializer
 )
 
 
 class BorrowingViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
     GenericViewSet
 ):
+    permission_classes = (IsAuthenticated,)
     queryset = Borrowing.objects.select_related("book", "user")
 
     def get_serializer_class(self):
@@ -21,5 +25,14 @@ class BorrowingViewSet(
             return BorrowingListSerializer
         if self.action == "retrieve":
             return BorrowingRetrieveSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
 
         return BorrowingSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+
+        return queryset

374d15a63e2e0999f8c586ac20708f07ec490ced
charmniovi <charmniovi@tutanota.com>
2024-06-14 13:34:41 +0300

    registered PaymentView endpoint

---
+++ payments/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from payments.views import PaymentView
+
+router = routers.DefaultRouter()
+router.register("", PaymentView, basename="payments-read")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "payments"

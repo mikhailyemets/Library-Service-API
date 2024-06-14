6fefaf7d505fd93bf9da38b89b0b085d5ba4c545
charmniovi <charmniovi@tutanota.com>
2024-06-14 22:07:26 +0300

    added select_related

--- payments/views.py
+++ payments/views.py
@@ -11,12 +11,15 @@ from payments.serializers import (
 
 class PaymentView(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     permission_classes = (IsAuthenticated,)
+    queryset = Payment.objects.all()
 
     def get_queryset(self):
         user = self.request.user
-        if user.is_staff or user.is_superuser:
-            return Payment.objects.all()
-        return Payment.objects.filter(borrowing__user=user)
+        if not (user.is_staff or user.is_superuser):
+            self.queryset = self.queryset.filter(borrowing__user=user)
+        self.queryset = self.queryset.select_related("borrowing__user")
+        self.queryset = self.queryset.select_related("borrowing__book")
+        return self.queryset
 
     def get_serializer_class(self):
         if self.action == 'list':

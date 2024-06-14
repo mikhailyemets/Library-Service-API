3928a24905a73d108b5efd667d41571274ea0b3e
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-06-14 14:05:03 +0200

    fixed flake8 issues

--- library_service/settings.py
+++ library_service/settings.py
@@ -37,12 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     # custom apps
     "books",
     "borrowings",
     "user",
-    
     # 3rd party apps
     "rest_framework",
 ]

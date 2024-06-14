ad42be5488989cec3361f8a9922fc17097f8d346
Serhii Marynin <serhii.marynin@gmail.com>
2024-06-14 15:28:08 +0300

    Fix flake errors

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -5,4 +5,11 @@ from borrowings.models import Borrowing
 
 @admin.register(Borrowing)
 class BorrowingAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "book", "borrow_date", "expected_return_date", "actual_return_date")
+    list_display = (
+        "id",
+        "user",
+        "book",
+        "borrow_date",
+        "expected_return_date",
+        "actual_return_date",
+    )
--- borrowings/models.py
+++ borrowings/models.py
@@ -9,8 +9,12 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
-    book = models.ForeignKey(Book, on_delete=models.RESTRICT, related_name="borrowings")
-    user = models.ForeignKey(get_user_model(), on_delete=models.RESTRICT, related_name="borrowings")
+    book = models.ForeignKey(Book, on_delete=models.PROTECT, related_name="borrowings")
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        related_name="borrowings"
+    )
 
     class Meta:
         ordering = ("expected_return_date", "borrow_date")

4aeaf65c9ff1fb35b6e4b861655af739d4a03506
charmniovi <charmniovi@tutanota.com>
2024-06-14 13:33:26 +0300

    added PaymentView

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import (
+    PaymentListSerializer,
+    PaymentRetrieveSerializer
+)
+
+
+class PaymentView(GenericViewSet, ListModelMixin, RetrieveModelMixin):
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff or user.is_superuser:
+            return Payment.objects.all()
+        return Payment.objects.filter(borrowing__user=user)
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PaymentListSerializer
+        if self.action == 'retrieve':
+            return PaymentRetrieveSerializer

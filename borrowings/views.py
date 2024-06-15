c786c0f94d1c750e476e9f64f8362ef5ffd6460f
Serhii Marynin <serhii.marynin@gmail.com>
2024-06-15 14:36:48 +0300

    Implement return_book action

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,5 +1,11 @@
+from datetime import date
+
+from django.db import transaction
 from rest_framework import mixins
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from borrowings.models import Borrowing
@@ -25,7 +31,7 @@ class BorrowingViewSet(
     def get_serializer_class(self):
         if self.action == "list":
             return BorrowingListSerializer
-        if self.action == "retrieve":
+        if self.action in ("retrieve", "return_book"):
             return BorrowingRetrieveSerializer
         if self.action == "create":
             return BorrowingCreateSerializer
@@ -38,3 +44,26 @@ class BorrowingViewSet(
             queryset = queryset.filter(user=self.request.user)
 
         return queryset
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="return"
+    )
+    def return_book(self, request, pk=None):
+        borrowing = self.get_object()
+        if borrowing.actual_return_date:
+            raise ValidationError(
+                {"actual_return_date": "Date already set, book returned"}
+            )
+
+        book = borrowing.book
+        serializer = self.get_serializer(borrowing)
+
+        with transaction.atomic():
+            book.inventory += 1
+            borrowing.actual_return_date = date.today()
+            book.save()
+            borrowing.save()
+
+        return Response(serializer.data)

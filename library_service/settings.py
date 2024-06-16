f0e8d18b8969ca1249d21dfb6d48a2490a37d855
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-06-16 21:12:35 +0200

    delete redefinition of unused 'timedelta'

--- library_service/settings.py
+++ library_service/settings.py
@@ -14,7 +14,6 @@ import sys
 from datetime import timedelta
 from pathlib import Path
 from celery.schedules import crontab
-from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

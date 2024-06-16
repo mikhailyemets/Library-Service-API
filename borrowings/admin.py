fff5904737538aa4e1b0a2f116e8c5f4767388a0
Serhii Marynin <serhii.marynin@gmail.com>
2024-06-16 14:59:53 +0300

    Add some explanations of borrowings on admin panel

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -13,3 +13,42 @@ class BorrowingAdmin(admin.ModelAdmin):
         "expected_return_date",
         "actual_return_date",
     )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "user",
+                    "book",
+                    "expected_return_date",
+                ),
+                "description": (
+                    "By creating instance from admin panel "
+                    "book inventory not decreased."
+                )
+            }
+        ),
+        (
+            "Return book",
+            {
+                "fields": ("actual_return_date",),
+                "description": (
+                    "By setting actual return date from admin panel "
+                    "book inventory not increased."
+                )
+            }
+        )
+
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        help_texts = {
+            "expected_return_date": (
+                "Minimum expected return date: tomorrow."
+            ),
+            "actual_return_date": (
+                "Setting this field mean that borrowed book returned."
+            ),
+        }
+        kwargs.update({"help_texts": help_texts})
+        return super().get_form(request, obj, **kwargs)

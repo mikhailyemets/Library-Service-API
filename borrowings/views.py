df0dccfc617c8508b3c1afa71551473123915dee
Serhii Marynin <serhii.marynin@gmail.com>
2024-06-16 15:09:22 +0300

    Implement borrowing list endpoint filtering by user_id and is_active parameters

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from django.db import transaction
+from django.db.models import Q
 from rest_framework import mixins
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
@@ -24,7 +25,11 @@ class BorrowingViewSet(
     mixins.CreateModelMixin,
     GenericViewSet
 ):
-    queryset = Borrowing.objects.select_related("book", "user")
+    queryset = (
+        Borrowing.objects
+        .annotate(is_active=Q(actual_return_date__isnull=True))
+        .select_related("book", "user")
+    )
     permission_classes = [IsAuthenticated, IsAdminOrOwner]
 
     def get_serializer_class(self):
@@ -39,8 +44,19 @@ class BorrowingViewSet(
 
     def get_queryset(self):
         queryset = self.queryset
-        if not self.request.user.is_staff:
-            queryset = queryset.filter(user=self.request.user)
+        user = self.request.user
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        if not user.is_staff:
+            queryset = queryset.filter(user=user)
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+        if is_active:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(is_active=True)
+            if is_active.lower() == "false":
+                queryset = queryset.filter(is_active=False)
 
         return queryset
 

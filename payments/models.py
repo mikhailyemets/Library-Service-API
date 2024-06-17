8ddc7d74356da35ea44f5e905bd5636e5fe450ab
charmniovi <charmniovi@tutanota.com>
2024-06-17 08:52:31 +0300

    added Payment.date_added

--- payments/models.py
+++ payments/models.py
@@ -10,9 +10,12 @@ class Payment(models.Model):
         PAYMENT = 'PAYMENT', 'Payment'
         FINE = 'FINE', 'Fine'
 
-    borrowing = models.OneToOneField('borrowings.Borrowing', on_delete=models.CASCADE)
+    borrowing = models.OneToOneField('borrowings.Borrowing',
+                                     on_delete=models.CASCADE,
+                                     related_name='payment')
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
     type = models.CharField(max_length=10, choices=Type.choices, default=Type.PAYMENT)
     session_url = models.URLField()
     session_id = models.CharField(max_length=100)
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+    date_added = models.DateField(auto_now=True)

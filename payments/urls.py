bf941a1e03f313d125d0d5c99c704b81e2abf5e3
charmniovi <charmniovi@tutanota.com>
2024-06-17 08:55:16 +0300

    registered SuccessPaymentView, CancelPaymentView endpoints

--- payments/urls.py
+++ payments/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from payments.views import PaymentView
+from payments.views import PaymentView, SuccessPaymentView, CancelPaymentView
 
 router = routers.DefaultRouter()
 router.register("", PaymentView, basename="payments")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("success/<int:pk>/", SuccessPaymentView.as_view(), name="success_payment"),
+    path("cancel/", CancelPaymentView.as_view(), name="cancel_payment"),
 ]
 
 app_name = "payments"

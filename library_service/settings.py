950f2f1148ef85eeb6453edbf2f22c71f81c635b
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-06-17 17:02:31 +0200

    add and set .env .env.example

--- library_service/settings.py
+++ library_service/settings.py
@@ -88,15 +88,6 @@ WSGI_APPLICATION = 'library_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# SQLITE3
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",

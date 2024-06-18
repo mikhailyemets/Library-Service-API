c6f6ccd44e6ab49645301e460a63a0de68d34b75
charmniovi <charmniovi@tutanota.com>
2024-06-18 11:12:31 +0300

    added management command to create 10 authors

---
+++ books/management/commands/fill_authors.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from books.models import Author
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        faker = Faker().unique
+        authors = [Author(first_name=faker.first_name(), last_name=faker.last_name()) for _ in range(10)]
+
+        Author.objects.bulk_create(authors, ignore_conflicts=True)

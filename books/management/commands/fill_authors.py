ae36623c9c31b06392db933846bb29302810aea1
charmniovi <charmniovi@tutanota.com>
2024-06-18 11:13:20 +0300

    replaced Author range creations

--- books/management/commands/fill_authors.py
+++ books/management/commands/fill_authors.py
@@ -7,6 +7,12 @@ from books.models import Author
 class Command(BaseCommand):
     def handle(self, *args, **options):
         faker = Faker().unique
-        authors = [Author(first_name=faker.first_name(), last_name=faker.last_name()) for _ in range(10)]
+        authors = [
+            Author(
+                first_name=faker.first_name(),
+                last_name=faker.last_name()
+            )
+            for _ in range(10)
+        ]
 
         Author.objects.bulk_create(authors, ignore_conflicts=True)
